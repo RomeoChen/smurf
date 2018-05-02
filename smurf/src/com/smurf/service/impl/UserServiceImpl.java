84d1a27f3420f99b1e0bab4db0b2633e4d15c50e
RomeoChen <18186483358@163.com>
2018-05-02 12:59:07 +0800

    123

--- smurf/src/com/smurf/service/impl/UserServiceImpl.java
+++ smurf/src/com/smurf/service/impl/UserServiceImpl.java
@@ -14,26 +14,32 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	UserMapper userMapper;
 
+	@Override
 	public List<User> list() {
 		return userMapper.list();
 	};
 
+	@Override
 	public void add(User user) {
 		userMapper.add(user);
 	}
 
+	@Override
 	public int delete(int id) {
 		return userMapper.delete(id);
 	}
 
+	@Override
 	public User getById(int id) {
 		return userMapper.getById(id);
 	}
 
+	@Override
 	public int update(User user) {
 		return userMapper.update(user);
 	}
 
+	@Override
 	public int count() {
 		return userMapper.count();
 	}
