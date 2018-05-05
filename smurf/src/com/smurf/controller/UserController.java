39ad214f9c5f10f73c458d4704f5d1c756f7a467
RomeoChen <18186483358@163.com>
2018-05-05 12:40:25 +0800

    delete 'update' and 'delete' these stupid process

--- smurf/src/com/smurf/controller/UserController.java
+++ smurf/src/com/smurf/controller/UserController.java
@@ -22,17 +22,6 @@ public class UserController {
 	@Autowired
 	UserService userService;
 
-	//所有用户列表
-	@RequestMapping("/listUser")
-	//http://localhost/smurf/user/listUser/
-	public ModelAndView listUser() {
-		ModelAndView mav = new ModelAndView();
-		List<User> list = userService.list();
-
-		mav.addObject("list", list);
-		mav.setViewName("listUser");
-		return mav;
-	}
 
 	//执行添加操作，并跳转至用户列表
 	@RequestMapping("/addUser")
@@ -56,51 +45,6 @@ public class UserController {
 		ModelAndView modelAndView = new ModelAndView("index");
 		return modelAndView;
 	}
-	
-	//跳转至更新用户界面
-	@RequestMapping("toUpdateUser")
-	public String toUpdateUser() {
-		return "/updateUser";
-	}
-	
-	//更改用户信息
-	@RequestMapping("/updateUser")
-	//http://localhost/smurf/user/updateUser/
-	public String updateUser(User user, HttpServletRequest request,Model model) {
-		if(userService.update(user)>0) {
-			//user = userService.getById(user.getId());
-			request.setAttribute("user", user);
-			model.addAttribute("user",user);
-			return "/listUser";
-		}else {
-			return "/updateError";
-		}
-	}
-	
-	@RequestMapping(value="/getUser") 
-	//http://localhost/smurf/user/gerUser?id=2
-    public ModelAndView getUser(int id){  
-         ModelAndView modelAndView = new ModelAndView();
-         User user = userService.getById(id);
-         modelAndView.addObject("user", user);
-         modelAndView.setViewName("updateUser");
-         return modelAndView;
-    }  
-	
-	@RequestMapping("/delUser")  
-    public void delUser(int id,HttpServletRequest request,HttpServletResponse response){  
-        String result = "{\"result\":\"error\"}";  
-        if(userService.delete(id)>0){  
-            result = "{\"result\":\"success\"}";  
-        }  
-        response.setContentType("application/json");  
-        try {  
-            PrintWriter out = response.getWriter();  
-            out.write(result);  
-        } catch (IOException e) {  
-            e.printStackTrace();  
-        }  
-    }
 	
 	@RequestMapping("/qrcode")
 	public ModelAndView qrcode() {
